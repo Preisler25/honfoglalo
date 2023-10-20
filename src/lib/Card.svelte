4c719f5d01fdb9ff87cd4d69f0e3294877ab2e0f
Preisler25 <preisler.andras@gmail.com>
2023-10-20 14:09:17 +0200

    main

---
+++ src/lib/Card.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Icon from '@iconify/svelte';
+  
+  export let title = "Please specify a title";
+  export let desc = "Please specify a description";
+  export let icon = "bx:bx-home";
+</script>
+
+<div class="bg-yellow-600 shadow-lg hover:border hover:border-slate-200 transition border border-yellow-600 box-content rounded-lg p-3">
+  <span class="flex flex-row items-center mb-2">
+    <Icon icon={icon} class="text-2xl mr-1" />
+    <h1 class="font-bold text-xl">{title}</h1>
+  </span>
+  <p>{desc}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Card from '$lib/Card.svelte';
+</script>
+
+<svelte:head>
+  <title>Honfoglaló</title>
+</svelte:head>
+
+<main class="bg-gray-800 min-h-screen text-white">
+  <h1 id="title" class="font-black text-center text-6xl p-4 shadow-lg">Honfoglaló</h1>
+  <div class="h-full  grid grid-rows-4 grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-3 mx-3">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+  
+</main>
