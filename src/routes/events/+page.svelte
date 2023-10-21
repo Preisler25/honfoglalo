f20cd61d468c25dbf42b4cc8b57ab8ef0d98bdf6
Berényi Bence <berenyibence98@gmail.com>
2023-10-21 15:39:58 +0200

    events page and slug for event view

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Card from "$lib/Card.svelte";
+</script>
+
+<svelte:head>
+    <title>Eseméynek</title>
+</svelte:head>
+
+<main class="bg-gray-800 min-h-screen text-white">
+    <h1 id="title" class="font-black text-center text-6xl p-4 shadow-lg">
+        Események
+    </h1>
+</main>
